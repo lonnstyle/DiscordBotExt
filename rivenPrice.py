0bee0ced480feb1d067c9c73132d540167f06d2e
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2020-12-01 18:00:37 +0800

    Update rivenPrice.py
    針對Prime後綴的優化

--- rivenPrice.py
+++ rivenPrice.py
@@ -21,54 +21,64 @@ class rivenPrice(Cog_Extension):
   
   def riven(self,name):
     Chinese= name
+    name = name.replace(" ","_")
+    # Euphona and Reaper has only prime variant
+    # prime_only = ["euphona_prime", "reaper_prime"]
+    # if name_lower not in prime_only:
+    name_lower = name.lower()
+    if name_lower != "euphona_prime" and name_lower != "reaper_prime":
+      name = name_lower.replace("_prime","")
+      name = name.replace("prime","")
     weapon = dict.get(name,"Empty")
     if weapon == "Empty":
-      return('Ordis...不懂怎麼翻譯')
+      weapon = name
     else:
       weapon = weapon.lower()
-      url = 'https://api.warframe.market/v1/auctions/search?type=riven&weapon_url_name=' + weapon + '&sort_by=price_asc'
-      html = requests.get(url)
-      if html.status_code != 200:
-        return('查到...Ordis發生錯誤...API出錯！')
-      else:
-        rivenData = json.loads(html.text)
-        rivenData = rivenData['payload']
-        rivenData = rivenData['auctions']
-        count = 0
-        message = f'以下為{Chinese}紫卡的查詢結果（按價格由低至高順序）\n'
-        for items in rivenData:
-          if count < 3:
-            owner = items['owner']
-            if owner['status'] != 'offline':
-              print(items)
-              rivenItem = items['item']
-              rivenName = rivenItem['name']
-              message += f'```\n紫卡名稱:{Chinese}{rivenName}\n'
-              ownerName = owner['ingame_name']
-              message += f'賣家:{ownerName}\n'
-              rerolls = rivenItem['re_rolls']
-              message += f'迴圈次數:{rerolls}\n'
-              for attr in rivenItem['attributes']:
-                attribute = attr['url_name']
-                attribute = attrDict.get(attribute,attribute)
-                value = attr['value']
-                if attr['positive'] == True:
-                  message += f'正面詞條:{attribute} {value}\n'
-                elif attr['positive'] == False:
-                  message += f'負面詞條:{attribute} {value}\n'
-              if items['top_bid'] == 'None':
-                top_bid = items['top_bid']
-                message += f'目前競標:{top_bid}\n'
+    print(weapon)
+    url = 'https://api.warframe.market/v1/auctions/search?type=riven&weapon_url_name=' + weapon + '&sort_by=price_asc'
+    html = requests.get(url)
+    if html.status_code != 200:
+      return('查到...Ordis發生錯誤...API出錯！')
+    else:
+      rivenData = json.loads(html.text)
+      rivenData = rivenData['payload']
+      rivenData = rivenData['auctions']
+      count = 0
+      message = f'以下為{Chinese}紫卡的查詢結果（按價格由低至高順序）\n'
+      for items in rivenData:
+        if count < 3:
+          owner = items['owner']
+          if owner['status'] != 'offline':
+            rivenItem = items['item']
+            rivenName = rivenItem['name']
+            message += f'```\n紫卡名稱:{Chinese} {rivenName}\n'
+            ownerName = owner['ingame_name']
+            message += f'賣家:{ownerName}\n'
+            rank = rivenItem['mod_rank']
+            rerolls = rivenItem['re_rolls']
+            message += f'等級:{rank}\n'
+            message += f'迴圈次數:{rerolls}\n'
+            for attr in rivenItem['attributes']:
+              attribute = attr['url_name']
+              attribute = attrDict.get(attribute,attribute)
+              value = attr['value']
+              if attr['positive'] == True:
+                message += f'正面詞條:{attribute} {value}\n'
+              elif attr['positive'] == False:
+                message += f'負面詞條:{attribute} {value}\n'
+            if items['top_bid'] == 'None':
+              top_bid = items['top_bid']
+              message += f'目前競標:{top_bid}\n'
+            else:
+              starting_price = items['starting_price']
+              buyout_price = items['buyout_price']
+              if starting_price == buyout_price:
+                message += f'價格:{buyout_price}\n'
               else:
-                starting_price = items['starting_price']
-                buyout_price = items['buyout_price']
-                if starting_price == buyout_price:
-                  message += f'價格:{buyout_price}\n'
-                else:
-                  message += f'起標價格:{starting_price}\n'
-                  message += f'買斷價格:{buyout_price}\n'
-              count += 1
-              message += '```'
+                message += f'起標價格:{starting_price}\n'
+                message += f'買斷價格:{buyout_price}\n'
+            count += 1
+            message += '```'
       return(message)
 
       
