ab820cc12602069abda8aa2a8286009b5fd740c7
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2020-12-06 17:31:11 +0800

    Minor update on baroManual.py
    display leave time of Baro

--- baroManual.py
+++ baroManual.py
@@ -5,6 +5,7 @@ import os
 import requests
 import json
 import asyncio
+import chinese_converter
 
 rawDict = requests.get("https://raw.githubusercontent.com/lonnstyle/riven-mirror/dev/src/i18n/lang/zh-Hant.json")
 Dict = json.loads(rawDict.text)
@@ -18,7 +19,13 @@ class baroManual(Cog_Extension):
     if html['active'] == True:
       message = "```"
       location = html['location']
-      await ctx.send(f"Baro Ki'Teer 已經到達{location},帶來的商品如下:")
+      location = chinese_converter.to_traditional(location)
+      stay = html['endString']
+      stay = stay.replace("d","天")
+      stay = stay.replace("h","小時")
+      stay = stay.replace("m","分鐘")
+      stay = stay.replace("s","秒")
+      await ctx.send(f"Baro Ki'Teer 已經到達{location},停留時間為{stay}\t帶來的商品如下:")
       for items in html['inventory']:
         item = items['item']
         item = item.lower()
@@ -35,18 +42,17 @@ class baroManual(Cog_Extension):
         name = Dict.get(name,name)
         ducats = items['ducats']
         credits = items['credits']
-        message += f"物品:{name}\t杜卡德金幣:{ducats}\t現金:{credits}\n"
+        message += f"物品:{name}\n杜卡德金幣:{ducats}\t現金:{credits}\n"
       message += "```"
       await ctx.send(message)
     if html['active'] == False:
-      timeLeft = html['endString']
-      day,minute,second = timeLeft.split(" ")
-      day = day.replace("d","天")
-      minute = minute.replace("m","分")
-      second = second.replace("s","秒")
-      timeLeft = day+minute+second
-      location = html['location']
-      await ctx.send(f"Baro Ki' Teer會在{timeLeft}後抵達{location}") 
+      arrive = html['endString']
+      arrive = arrive.replace("d","天")
+      arrive = arrive.replace("h","小時")
+      arrive = arrive.replace("m","分鐘")
+      arrive = arrive.replace("s","秒")
+      arrive = html['location']
+      await ctx.send(f"Baro Ki' Teer會在{arrive}後抵達{location}") 
 
 
 def setup(bot):
