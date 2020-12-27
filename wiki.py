360495a62ffa578b174f3e0d2904f4312649a992
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2020-12-27 12:32:00 +0800

    嵌入鏈接優化

--- wiki.py
+++ wiki.py
@@ -7,6 +7,8 @@ zh = Site('warframe.huijiwiki.com', scheme='https')
 tc = Site('warframe.fandom.com', path='/zh-tw/', scheme='https')
 en = Site('warframe.fandom.com', path='/', scheme='https')
 
+subpage = {"Main":"概述","Prime":"Prime","Abilities":"技能","Equip":"可替換裝備","Patch_History":"更新歷史","Media":"影音資料"}
+
 class wiki(Cog_Extension):
   @commands.command(name='wiki',aliases=['維基'])
   async def wiki(self,ctx,*args):
@@ -23,25 +25,41 @@ class wiki(Cog_Extension):
           page = page.resolve_redirect()
           name = page.name
           url = "https://warframe.fandom.com/wiki/"+name
+          footer="英文Fandom"
+          host = en
       else:
         page = page.resolve_redirect()
         name = page.name
         url = "https://warframe.fandom.com/zh-tw/wiki/"+name
+        footer="繁中Fandom"
+        host = tc
     else:
       page = page.resolve_redirect()
       name = page.name
       url = "https://warframe.huijiwiki.com/wiki/"+name
+      footer="灰機Wiki"
+      host = zh
     url = url.replace(" ","_")
-    found = False
-    desc = "以下為相關頁面鏈接:\n"
+    found = 0
+    desc = "以下為嵌入頁面鏈接:\n"
+    for items in subpage:
+      sub = host.pages[f"{name}/{items}"]
+      if sub.exists == True:
+        linkURL = url.replace(name,"")+items
+        desc += f"[{subpage[items]}]({linkURL})\n"
+    if desc == "以下為嵌入頁面鏈接:\n":
+      desc = ""
+    desc += "以下為相關頁面鏈接:\n"
     for link in page.links():
       if name in link.name:
-        linkURL = url.replace(name,"")+"/"+link.name.replace(" ","_")
-        desc += f"[{link.name}]({linkURL})\n"
-        found = True
-    if found == False:
+        linkURL = url.replace(name,"")+link.name.replace(" ","_")
+        if found <=5:
+          desc += f"[{link.name}]({linkURL})\n"
+        found += 1
+    if found == 0:
       desc = ""
     embed = discord.Embed(title=name,url=url,description=desc)
+    embed.set_footer(text=footer)
     await ctx.send(embed=embed)
 
 
