8be89034c5c90dcd85c3332b5cbca48554b64201
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2021-03-18 21:30:42 +0800

    修正投票功能
    新增派發身份組功能作為pre-release內容

--- cmds/common.py
+++ cmds/common.py
@@ -4,10 +4,17 @@ from core.classes import Cog_Extension
 import random
 import json
 from random import randint
+import requests
+import re
+import asyncio
 
 
 with open('setting.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
+
+emoji = requests.get("http://gist.githubusercontent.com/Vexs/629488c4bb4126ad2a9909309ed6bd71/raw/416403f7080d1b353d8517dfef5acec9aafda6c3/emoji_map.json").text
+emoji = json.loads(emoji)
+emoji = {x: y for y, x in emoji.items()}
 
 class common(Cog_Extension):
     tag = "common"
@@ -27,17 +34,53 @@ class common(Cog_Extension):
         await ctx.message.delete()
         embed=discord.Embed(description=msg,color=0x3C879C)
         message = await ctx.send(embed=embed)
+        #此功能為開發伺服器上公告自動廣播之用
+        if ctx.channel.id == 820571680479903776:
+          await message.publish()
     
     @commands.command(name= 'poll', aliases=['投票'],brief="發起投票",description="讓機器人發起一項投票")
     async def poll(self,ctx,topic,option1,emoji1,option2,emoji2):
         await ctx.message.delete()
-        embed=discord.Embed(description=topic,color=0x3C879C)
-        embed.add_field(name=option1,value=emoji1)
-        embed.add_field(name=option2,value=emoji2)
-        message = await ctx.send(embed=embed)
-        await message.add_reaction(emoji1)
-        await message.add_reaction(emoji2)
+        match1 = re.match(r'<(a?):([a-zA-Z0-9\_]+):([0-9]+)>$', emoji1)
+        match2 = re.match(r'<(a?):([a-zA-Z0-9\_]+):([0-9]+)>$', emoji2)
+        if (emoji.get(emoji1,None)!=None or match1) and (emoji.get(emoji2,None)!=None or match2):
+          if emoji1 == emoji2 or option1 == option2:
+            await ctx.send(embed=discord.Embed(title="錯誤信息",description='兩個一樣你要投什麼?',color=0xff0000))
+            return
+          embed=discord.Embed(description=topic,color=0x3C879C)
+          embed.add_field(name=option1,value=emoji1)
+          embed.add_field(name=option2,value=emoji2)
+          message = await ctx.send(embed=embed)
+          await message.add_reaction(emoji1)
+          await message.add_reaction(emoji2)
+        else:
+          await ctx.send(embed=discord.Embed(title="錯誤信息",description='這真的是emoji嗎?'))
 
+    @commands.command(name="role",brief="新增身份組",description="新增機器人可以分發的身份組")
+    async def role(self,ctx,role):
+      if ctx.message.author.id == ctx.guild.owner_id:
+          for roles in ctx.guild.roles:
+            if roles.name == role:
+              embed = discord.Embed(title="已新增身份組",description=f"已新增<@&{roles.id}>")
+              await ctx.send(embed=embed)
+              with open("roles.txt","a",encoding="utf8") as role_id:
+                role_id.write(roles.name+'\n')
+
+    @commands.command(name="join",brief="加入身份組",description="使機器人分配身份組權限")
+    async def join(self,ctx,role):
+      await ctx.message.delete()
+      with open("roles.txt","r",encoding="utf8") as roles:
+        if (role+'\n') in roles.readlines():
+          for roles in ctx.guild.roles:
+            if roles.name == role:
+              await ctx.author.add_roles(roles)
+              message = await ctx.send(embed=discord.Embed(title="已加入身份組",description=f"<@&{roles.id}>",color=0x00ff00))
+              await asyncio.sleep(10)
+              message.delete()
+        else:
+          message = await ctx.send(embed=discord.Embed(title="加入身份組失敗",description="請確保身份組輸入無誤",color=0xff0000))
+          await asyncio.sleep(10)
+          message.delete()
     
 def setup(bot):
     bot.add_cog(common(bot))
