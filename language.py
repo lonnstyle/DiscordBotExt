6f299ee1d8df1f2bce26e22452e7c0c2e8192d02
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2021-05-09 17:39:46 +0800

    v2.1.1
    Provide Simplified Chines as language option

--- language.py
+++ language.py
@@ -4,11 +4,14 @@ import requests
 class language():
   def __init__(self):
     self.zh_hant = json.loads(requests.get("https://raw.githubusercontent.com/lonnstyle/DiscordBotExt/main/zh-hant.json").text)
+    self.zh_hans = json.loads(requests.get("https://raw.githubusercontent.com/lonnstyle/DiscordBotExt/main/zh-hans.json").text)
     self.pref = json.load(open("setting.json",'r',encoding='utf8'))
     self.pref = self.pref.get("language","zh-hant")
   def langpref(self):
     if self.pref == 'zh-hant':
       return self.zh_hant
+    elif self.pref == 'zh-hans':
+      return self.zh_hans
     else:
       return json.load(open(self.pref,"r",encoding='utf8'))
       
