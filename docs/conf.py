0df9f4e663dee706def6df3dcd2709b9fda8eefe
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2021-06-07 22:07:23 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = 'v2.3.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser','sphinx.ext.autosectionlabel']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
