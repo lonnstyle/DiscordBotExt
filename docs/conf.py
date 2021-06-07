4501734a7606872b69895a859580fafea5f679c0
lonnstyle <lonnstyle@gmail.com>
2021-06-07 11:37:19 +0800

    docs test

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ release = 'v2.3.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -38,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. DiscordBotExtension documentation master file, created by
-   sphinx-quickstart on Mon Jun  7 10:49:26 2021.
+   sphinx-quickstart on Mon Jun  7 11:20:57 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
@@ -8,7 +8,8 @@ Welcome to DiscordBotExtension's documentation!
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+   
+   search
 
 
 
