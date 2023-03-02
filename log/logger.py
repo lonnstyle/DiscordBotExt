4b1c439ddc9d42f871cc15df0d8a64fde4e2ac09
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2023-03-02 10:33:02 +0800

    fix: duplicate definition of logger

--- log/logger.py
+++ log/logger.py
@@ -7,16 +7,21 @@ class logger():
         # clear log records
         with open('runtime.log','w') as log:
             pass
+        self.logger = []
         
 
     def getLogger(self,name,level=-1):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
+        for logger in self.logger:
+            if logger.name == name:
+                return logger
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
         file_handler = logging.FileHandler(filename='log/runtime.log', encoding='utf-8', mode='a')
         file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(lineno)d: %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
-        self.logger.addHandler(file_handler)
+        logger.addHandler(file_handler)
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(lineno)d: %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
-        self.logger.addHandler(stream_handler)
-        return self.logger
+        logger.addHandler(stream_handler)
+        self.logger.append(logger)
+        return logger
 
